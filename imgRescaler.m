c92161294e4de3c504e8a1a57b2425049e23542b
Nick H <nzhauger@gmail.com>
2024-10-11 13:00:52 -0700

    Update imgRescaler.m
    We're able to resize and zeropad the red channel. Now we just need to write the loops for green/blue.

--- imgRescaler.m
+++ imgRescaler.m
@@ -1,32 +1,37 @@
 %Nick Hauger 9OCT24
 %
 % ZED resolution: 640x360 (16:9) width x height
+% This only works if you 
 clear variables; clc;
-desrow = 640; % desired row and desired column size
-descol = 360;
+descol = 640; % desired row and desired column size
+desrow = 360;
 
 % this is just what we wrote on the board.
 orig = imread("test.jpg");
-row = size(orig,1); % note: size returns [row, col, # of color channels]
-col = size(orig,2);
 
-edit = orig; %making a copy of the original image called edit
-%this flips the image if its portait into landscape.
-if col < row
-    edit = imrotate(edit, 90); %transpose the edit matrix
-    temp = row; row = col; col = temp; %swap rol and col (theyre #s)
-end
+A2_scaled = imresize(orig,[desrow, NaN]);
 
-red = edit(1:row,1:col,1); % col 3 is 1 - the red channel
+A2_red = A2_scaled(:,:,1); 
 
-imshow(red) % this is just checking it works
+pad = zeros(desrow,descol-length(A2_red(1,:)));
 
-rratio = row/desrow % ratio of img's rows to desired rows
-cratio = col/descol % ratio of img's cols to desired cols
-if rratio > cratio
-    resized = imresize(edit, [360, NaN]);
-else 
-    resized = imresize(edit, [NaN, 640]);
-end
+A2_comb_red(1:desrow,1:length(A2_red(1,:))) = A2_red;
+A2_comb_red = [A2_comb_red,pad];
+imshow(A2_comb_red)
 
-imwrite(resized,'r.jpg')
+
+% %%%%%%%% Unneeded %%%%%%%
+% edit = orig; %making a copy of the original image called edit
+% %this flips the image if its portait into landscape.
+% if col < row
+%     edit = imrotate(edit, 90); %transpose the edit matrix
+%     temp = row; row = col; col = temp; %swap row and col (theyre #s)
+% end
+% 
+% rratio = row/desrow; % ratio of img's rows to desired rows
+% cratio = col/descol; % ratio of img's cols to desired cols
+% if rratio < cratio
+%     resized = imresize(edit, [descol, NaN]);
+% else 
+%     resized = imresize(edit, [NaN, desrow]);
+% end
