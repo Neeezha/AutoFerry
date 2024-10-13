2a207479bae8b533d18ad90341301cb23bc771d7
Nick H <nzhauger@gmail.com>
2024-10-12 19:43:05 -0700

    Update imgRescaler.m
    it now takes a single image, resizes it to desired dimensions.

--- imgRescaler.m
+++ imgRescaler.m
@@ -1,37 +1,62 @@
 %Nick Hauger 9OCT24
 %
 % ZED resolution: 640x360 (16:9) width x height
-% This only works if you 
+% This only works if you have 4:3 aspect original image coming in.
+% but we can edit it to work with any image by measuring the ratio.
 clear variables; clc;
 descol = 640; % desired row and desired column size
 desrow = 360;
 
-% this is just what we wrote on the board.
-orig = imread("test.jpg");
+orig = imread("test.jpg"); %create an array of data from the test jpg
 
-A2_scaled = imresize(orig,[desrow, NaN]);
+[row,col,ignore] = size(orig); % we need to grab ignore or this fn breaks
+%this flips the image if its portait into landscape.
+if col < row
+    orig = imrotate(orig, 90); %flip the portait 90 deg to landscape it
+end
 
-A2_red = A2_scaled(:,:,1); 
+ %scale it down to desired rows (assume its in landscape and originally 4:3)
+scaled = imresize(orig,[desrow, NaN]);
 
-pad = zeros(desrow,descol-length(A2_red(1,:)));
+red = scaled(:,:,1); %grabbing the red matrix values from scaled
 
-A2_comb_red(1:desrow,1:length(A2_red(1,:))) = A2_red;
-A2_comb_red = [A2_comb_red,pad];
-imshow(A2_comb_red)
+%creates a zero-pad vector to fill in the end of our image to make it 640
+pad = zeros(desrow,descol-length(red(1,:))); 
 
+% creating a vector with red's values in the first ???x360 positions
+% and pad's zero-padding in the last ???+1:640x360 positions.
+red = [red,pad]; %vector concatenating pad to the end of red. 
+%imshow(red)
 
-% %%%%%%%% Unneeded %%%%%%%
+% Now repeating for green and blue values.
+green = scaled(:,:,2); %grabbing the green matrix values from scaled
+green = [green,pad];%vector concatenating pad to the end. 
+%imshow(green)
+
+blue = scaled(:,:,3); %grabbing the blue matrix values from scaled
+blue = [blue,pad];%vector concatenating pad to the end. 
+%imshow(blue)
+
+% Now we combine the values of the three vectors into one, 'combo'.
+% Each channel gets its corresponding color vector.
+combo(1:desrow,1:descol,1) = red;
+combo(1:desrow,1:descol,2) = green;
+combo(1:desrow,1:descol,3) = blue;
+imshow(combo)
+imwrite(combo, 'resized.jpg') %and we write it.
+% 
+% We'll have to work more to automate this process through an entire folder
+% Like, take the original username and overwrite it with resized combo?
+% as long as the person running this keeps a copy. of the original.
+
+% %%%%%%%% Unneeded ratio checking. %%%%%%%
+%%%%% We will need this if images we use aren't 4:3 %%%%%
 % edit = orig; %making a copy of the original image called edit
-% %this flips the image if its portait into landscape.
-% if col < row
-%     edit = imrotate(edit, 90); %transpose the edit matrix
-%     temp = row; row = col; col = temp; %swap row and col (theyre #s)
-% end
 % 
 % rratio = row/desrow; % ratio of img's rows to desired rows
 % cratio = col/descol; % ratio of img's cols to desired cols
 % if rratio < cratio
-%     resized = imresize(edit, [descol, NaN]);
+%     resized = imresize(edit, [desrow, NaN]);
 % else 
-%     resized = imresize(edit, [NaN, desrow]);
+%     resized = imresize(edit, [NaN, descol]);
 % end
